3ab5c55bd3ff8ea709bf01deba0de89908d0d81f
Sankaranarayanan-P <sankaranarayananp2021@gmail.com>
2022-09-26 10:39:50 +0530

    new changes

--- routes/chat.js
+++ routes/chat.js
@@ -2,11 +2,19 @@ var express = require("express");
 var router = express.Router();
 
 const AWS = require("aws-sdk");
-const { response } = require("express");
-const IVSChat = new AWS.Ivschat();
+const IVSChat = new AWS.Ivschat({ region: "ap-south-1" });
 
-/* GET home page. */
-router.post("/auth", async function (req, res, next) {
+const response = {
+  statusCode: 200,
+  headers: {
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "POST",
+  },
+  body: "",
+};
+
+/* TO CREATE CHAT TOKEN */
+router.post("/auth", async function (req, res) {
   const body = req.body;
   const { arn, roomIdentifier, userId } = body;
   const roomId = arn || roomIdentifier;
@@ -39,8 +47,42 @@ router.post("/auth", async function (req, res, next) {
   } catch (err) {
     console.error("ERROR: chatAuthHandler > IVSChat.createChatToken:", err);
     response.statusCode = 500;
-    response.body = err.stack;
+    response.body = err.message;
   }
+  console.info(
+    `response from: ${req.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return res.status(response.statusCode).json(response.body);
+});
+
+/* TO HANDLE CHAT EVENT */
+router.post("/event", async (req, res) => {
+  const body = req.body;
+  const { arn, eventAttributes, eventName } = body;
+  // Construct parameters.
+  // Documentation is available at https://docs.aws.amazon.com/ivs/latest/ChatAPIReference/Welcome.html
+  const params = {
+    roomIdentifier: `${arn}`,
+    eventName: eventName,
+    attributes: { ...eventAttributes },
+  };
+
+  try {
+    await IVSChat.sendEvent(params).promise();
+    console.info("chatEventHandler > IVSChat.sendEvent > Success");
+    response.statusCode = 200;
+    // If sendEvent() is successfull, it will return an empty response.
+    // For the purposes of this API however, let's return "success" in the response body
+    response.body = {
+      arn: `${arn}`,
+      status: "success",
+    };
+  } catch (err) {
+    console.error("ERROR: chatEventHandler > IVSChat.sendEvent:", err);
+    response.statusCode = 500;
+    response.body = err.message;
+  }
+
   console.info(
     `response from: ${req.path} statusCode: ${response.statusCode} body: ${response.body}`
   );
